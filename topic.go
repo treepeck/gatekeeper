b360ab4541ae12b7e884943a2848af0db613b72b
Artem Bielikov <artem.bielikov@treepeck.com>
2025-08-14 20:27:31 +0300

    handle websocket connections
    The hub now can recieve and maintain a long-lived connections
    by receiving and sending Ping/Pong messages.

--- topic.go
+++ topic.go
@@ -1,84 +1,26 @@
-package pubsub
+package main
 
-import "log"
-
-type Topic struct {
-	// Topic Id.
-	Id string
-	// Subscribed actors.
-	subs map[*Actor]struct{}
-	// handlers will handle the incomming events.
-	handlers map[Action]Handler
-	// Register a new subscriber.
-	Register chan *Actor
-	// Unregister an existing subscriber.
-	Unregister chan *Actor
-	// Publish channel will recieve the events from the subscribed actors.
-	Publish chan Event
-	// Close will stop the RouteEvents routine.
-	Destroy chan bool
+type topic struct {
+	id   string
+	subs map[*client]struct{}
 }
 
-func NewTopic(Id string, handlers map[Action]Handler) *Topic {
-	return &Topic{
-		Id:         Id,
-		subs:       make(map[*Actor]struct{}),
-		handlers:   make(map[Action]Handler),
-		Register:   make(chan *Actor),
-		Unregister: make(chan *Actor),
-		Publish:    make(chan Event),
-		Destroy:    make(chan bool),
+func newTopic(id string) *topic {
+	return &topic{
+		id:   id,
+		subs: make(map[*client]struct{}),
 	}
 }
 
-func (t *Topic) RouteEvents() {
-	for {
-		select {
-		case a := <-t.Register:
-			t.register(a)
-
-		case a := <-t.Unregister:
-			t.unregister(a)
-
-		case e := <-t.Publish:
-			if h, exists := t.handlers[e.Action]; exists {
-				if data, err := h(e); err == nil {
-					t.publish(data)
-				}
-			}
-
-		case <-t.Destroy:
-			return
-		}
-	}
+func (t *topic) subscribe(c *client) {
+	t.subs[c] = struct{}{}
 }
 
-// register registers a new subscriber.
-func (t *Topic) register(a *Actor) {
-	t.subs[a] = struct{}{}
-
-	if data, err := t.handlers[ADD_SUB](Event{Pub: a}); err == nil {
-		t.publish(data)
-	}
-
-	log.Printf("actor \"%s\" registered in topic \"%s\"", a.Id, t.Id)
+func (t *topic) unsubscribe(c *client) {
+	delete(t.subs, c)
 }
 
-// unregister unregisters an existing subsciber.
-func (t *Topic) unregister(a *Actor) {
-	if _, exists := t.subs[a]; exists {
-		t.subs[a] = struct{}{}
-
-		if data, err := t.handlers[REMOVE_SUB](Event{Pub: a}); err == nil {
-			t.publish(data)
-		}
-
-		log.Printf("actor \"%s\" ubregistered from topic \"%s\"", a.Id, t.Id)
-	}
-}
-
-func (t *Topic) publish(data []byte) {
-	for a := range t.subs {
-		a.Recieve <- data
-	}
+func (t *topic) isSub(c *client) bool {
+	_, exists := t.subs[c]
+	return exists
 }
