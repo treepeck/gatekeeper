95fae1b9d0aea2c344b1d2a142bdf4b670fe8ba9
Artem Bielikov <artem.bielikov@treepeck.com>
2025-08-27 17:57:33 +0300

    authenticate handshake requests
    Authentication is handled by the core server.  The Gatekeeper sends an
    HTTP POST request with the session id extracted from the cookie and
    waits for a response containing the valid player id, which is then
    associated with the client connection.

--- internal/ws/handshake.go
+++ internal/ws/handshake.go
@@ -1,8 +1,10 @@
 package ws
 
 import (
-	"crypto/rand"
+	"bytes"
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/websocket"
 )
@@ -20,10 +22,48 @@ var upgrader = websocket.Upgrader{
 }
 
 /*
-HandleHandshake handles incomming WebSocket requests, upgrades the HTTP
-connection to the WebSocket protocol and registers the client to the [Server].
+HandleHandshake handles incoming WebSocket handshake requests.  It upgrades the
+HTTP connection to the WebSocket protocol and stores the [client].
+
+During the handshake, the [Server] performs authentication by sending an HTTP
+POST request to the endpoint specified in the AUTH_URL environment variable.
+The request body contains the session id, extracted from the client's cookie.
+
+The authentication endpoint must validate the session by looking it up in the
+session database.  If valid, the endpoint must return the corresponding player
+id, which is then associated with the client connection.
 */
 func HandleHandshake(rw http.ResponseWriter, r *http.Request, s *Server) {
+	if len(r.Cookies()) != 1 || r.Cookies()[0].Name != "Auth" {
+		http.Error(rw, "Unauthorized request.", http.StatusUnauthorized)
+		return
+	}
+
+	sessionId := r.Cookies()[0].Value
+
+	req, err := http.NewRequest(
+		http.MethodPost,
+		os.Getenv("AUTH_URL"),
+		bytes.NewReader([]byte(sessionId)),
+	)
+	if err != nil {
+		http.Error(rw, "Unauthorized request.", http.StatusUnauthorized)
+		return
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		http.Error(rw, "Unauthorized request.", http.StatusUnauthorized)
+		return
+	}
+
+	playerId, err := io.ReadAll(res.Body)
+	if err != nil || len(sessionId) != 32 {
+		http.Error(rw, "Unauthorized request.", http.StatusBadRequest)
+		return
+	}
+	res.Body.Close()
+
 	roomId := r.URL.Query().Get("rid")
 
 	conn, err := upgrader.Upgrade(rw, r, nil)
@@ -32,7 +72,7 @@ func HandleHandshake(rw http.ResponseWriter, r *http.Request, s *Server) {
 		return
 	}
 
-	c := newClient(rand.Text(), roomId, s, conn)
+	c := newClient(string(playerId), roomId, s, conn)
 
 	s.register <- c
 }
